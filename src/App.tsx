f1f2d8f76331d9f8fbf26c9f8162bb0a5e7f7a08
flori2203 <flori2232@gmail.com>
2022-09-08 10:24:38 +0000

    Added React-Router and empty Main-Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
+import MainPage from "./pages/MainPage";
 
-const App: React.FC = () => <div></div>;
+const App: React.FC = () => <MainPage></MainPage>;
 
 export default App;
---
+++ src/pages/MainPage/MainPage.tsx
@@ -0,0 +1,8 @@
+import {Typography} from "@mui/material";
+import React from "react";
+
+const MainPage = () => {
+    return <Typography>Test</Typography>;
+};
+
+export default MainPage;
